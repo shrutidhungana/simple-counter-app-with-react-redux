ea328f3f5e25e1fe5d424119abd3f41846106659
shrutidhungana <shrutidhungana@prixa.net>
2022-11-28 13:30:34 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {useSelector, useDispatch} from 'react-redux'
+
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  const increment = () => { 
+    dispatch({type:'INC'})
+  }
+  const decrement = () => {
+    dispatch({type:'DEC'})
+  }
+
+  const addBy = () => {
+    dispatch({type:'ADD', payload: counter})
+  }
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick = {addBy}>Add value</button>
+    </div>
+  );
+}
+
+export default App;
